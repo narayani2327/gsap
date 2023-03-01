8777e6f8ee92567a2970785a09cce27b823861ea
narayani2327 <harikalanarayani@gmail.com>
2023-03-01 16:43:53 +0530

    Update parallax.js

--- parallax.js
+++ parallax.js
@@ -2,6 +2,14 @@ gsap.registerPlugin(ScrollTrigger)
 // toggleActions:"play none none none"
 // play pause complete reverse start rest none restart
 // onEnter onLeave onEnterBack onLeaveBack
+
+gsap.from(".logo",{
+  scale:3,
+  duration:2,
+  opacity:0,
+  scrollTrigger:".logo"
+})
+
 gsap.from("h2",{
   // x:-700,
   scale:2,
@@ -59,17 +67,37 @@ gsap.from(".three",{
     start:"top 80%",
     end:"top 10%",
     toggleActions:"restart reverse restart none",
-    markers:{
-      startColor:"purple",
-      endColor:"fuchsia",
-      fontsize:"3rem"
-    }
+    // markers:{
+    //   startColor:"purple",
+    //   endColor:"fuchsia",
+    //   fontsize:"3rem"
+    // }
   }
 })
-gsap.from(".info",{
+gsap.from(".info p",{
   x:700,
-  delay:3,
-  duration:3,
+  // delay:3,
+  duration:1,
   opacity:0,
-  scrollTrigger:".info"
+  scrollTrigger:{
+    trigger:".info",
+    start:"top 80%",
+    end:"top 10%",
+    toggleActions:"restart reverse restart none",
+  }
 })
+gsap.from(".info img",{
+  scale:1,
+  // delay:3,
+  duration:1,
+  opacity:0,
+  scrollTrigger:{
+    trigger:".info",
+    start:"top 80%",
+    end:"top 10%",
+    toggleActions:"restart reverse restart none",
+  }
+})
+
+
+
